ecbdee155387d563a330e103f4a5eba04c6ec2ec
John Doe <christianchungcc72574@utexas.edu>
2024-10-24 12:04:43 -0500

    Added new User button. Stylized login page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
-import React from "react";
+import React, { usestate } from "react";
 import Login from "./Login";
 
 function App() {
   return (
-    <div className="App">
-      <Login />
-    </div>
+    // <div className="App">
+    //   <Login />
+    // </div>
+    <div className="login-container">
+    {/* <img src={require('./logo.png')} alt="Logo" className="logo" /> */}
+    <h2>Login</h2>
+    <form>
+      <label>Username:</label>
+      <input type="text" />
+      <label className="password">Password:</label>
+      <input type="password" />
+      <button type="submit">Login</button>
+    </form>
+    <button className="create-user-button">Create New User</button> {/* New button */}
+  </div>
   );
 }
 

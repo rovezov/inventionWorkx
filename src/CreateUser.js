43931ae7eaf18ebeae4a7aecdf292e8904842480
John Doe <christianchungcc72574@utexas.edu>
2024-10-24 21:18:29 -0500

    Added Login funcitonality. Currently using local login variables. Successful login navigates to project dashboard page.

---
+++ src/CreateUser.js
@@ -0,0 +1,13 @@
+// CreateUser.js
+import React from 'react';
+
+function CreateUser() {
+  return (
+    <div>
+      <h2>Create New User</h2>
+      {/* Your form or other content goes here */}
+    </div>
+  );
+}
+
+export default CreateUser;

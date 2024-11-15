c65de42e7c9096bcd486ea9b965c7c65756aa8ea
Resul Ovezov <resul_ovezov@utexas.edu>
2024-11-14 18:18:54 -0600

    wow

--- backend/utils/run_backend.py
+++ backend/utils/run_backend.py
@@ -10,7 +10,7 @@ from api.hardware_api import hardware_blueprint
 
 
 app = Flask(__name__)
-CORS(app, origins="http://3.144.236.59")
+CORS(app)
 
 # Register blueprints for each API
 app.register_blueprint(auth_blueprint, url_prefix="/api/auth")
